6a8a431f873fd337fc3c84a7ac5962d095b9a3e6
jun.kishimoto <ksmtjn@gmail.com>
2019-04-30 15:31:36 +0000

    follow, unfollow

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,7 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session[:usser_id] = nil
+   session[:user_id] = nil
     flash[:success] = 'ログアウトしました。'
     redirect_to root_url
   end
